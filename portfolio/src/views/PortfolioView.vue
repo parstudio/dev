f7b89fd88c61e9d86fc38104b53d2b426c4158c4
jh.park <parstudio.kr@gmail.com>
2022-06-18 02:22:15 +0900

    포트폴리오 레이아웃 추가 설정

--- portfolio/src/views/PortfolioView.vue
+++ portfolio/src/views/PortfolioView.vue
@@ -2,6 +2,9 @@
 	<div id="portfolio-wrapper">
 		<div class="left-container">
 			<div class="category">
+				<span class="logo">
+					<router-link to="/">Par Studio</router-link>
+				</span>
 				<span>Still Life</span>
 				<span>Beauty</span>
 				<span>Food</span>
@@ -41,21 +44,33 @@
 			</div>
 		</div>
 		<div class="right-container">
-			<div class="detail">
+			<button>
+				<span>INFO</span>
+			</button>
+			<div class="detail" @mousewheel="contentWheelEvent" ref="content">
 				<div class="content">
 					<img src="@/assets/CLIO/210901K_CLIO-0024.jpg" alt="img1" />
-					<img src="@/assets/CLIO/210901K_CLIO-0024.jpg" alt="img1" />
-					<img src="@/assets/CLIO/210901K_CLIO-0024.jpg" alt="img1" />
-					<img src="@/assets/CLIO/210901K_CLIO-0024.jpg" alt="img1" />
-					<img src="@/assets/CLIO/210901K_CLIO-0024.jpg" alt="img1" />
-					<img src="@/assets/CLIO/210901K_CLIO-0024.jpg" alt="img1" />
+					<img src="@/assets/CLIO/210901K_CLIO-4155.jpg" alt="img1" />
+					<img src="@/assets/CLIO/210901K_CLIO-4177.jpg" alt="img1" />
+					<img src="@/assets/CLIO/210901K_CLIO-4192.jpg" alt="img1" />
+					<img src="@/assets/CLIO/210901K_CLIO-4208.jpg" alt="img1" />
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const content = ref();
+const SPEED = 1.5;
+
+const contentWheelEvent = (event) => {
+	const y = parseInt(event.deltaY * SPEED);
+	content.value.scrollLeft += y;
+};
+</script>
 
 <style scoped>
 @import "@/css/portfolio.css";
