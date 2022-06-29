4f5953d1ec0cffc4990ddc1cbbdf527c3291b17e
jh.park <parstudio.kr@gmail.com>
2022-06-29 23:23:48 +0900

    풀 확인

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,6 +2,7 @@
 	<!-- https://parstudiokr.wixsite.com/parstudio 포트폴리오사이트-->
 	<!-- https://saybyuk.com/  참고사이트-->
 	<!-- http://o-r-k-r.kr/ 참고사이트-->
+	<!-- 220629 김혜련 잠들다. -->
 	<router-view></router-view>
 </template>
 
