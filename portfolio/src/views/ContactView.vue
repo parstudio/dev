1a688711996a9e23e2adaf1cec61794f208146c6
alieen97 <93559394+alieen97@users.noreply.github.com>
2022-07-07 23:20:09 +0900

    .contact 추가

--- portfolio/src/views/ContactView.vue
+++ portfolio/src/views/ContactView.vue
@@ -3,22 +3,31 @@
 		<h1 class="title">Contact Us</h1>
 		<div class="outer">
 			<div class="left">
+				<div class="hoverBack"></div>
 				<div class="leftBox">
 					<ul>
-						<li>+82 10 4560 0488</li>
-						<li><a href="parstudio.kr@gmail.com">parstudio.kr@gmail.com</a></li>
+						<li>
+							<p>+82 10 4560 0488</p>
+							<span class="underLine"></span>
+						</li>
+						<li>
+							<a href="parstudio.kr@gmail.com">parstudio.kr@gmail.com</a>
+							<span class="underLine"></span>
+						</li>
 						<li>
 							<a href="https://www.instagram.com/parstudio.kr/">
 								@parstudio.kr
 							</a>
+							<span class="underLine"></span>
 						</li>
 					</ul>
 				</div>
 			</div>
 			<div class="right">
+				<div class="hoverBack"></div>
 				<div class="inputWrap">
 					<input type="text" placeholder="enter your name..." />
-					<textarea cols="50" rows="10"></textarea>
+					<textarea cols="50" rows="13"></textarea>
 					<input type="submit" value="Submit" />
 					<!--<router-link to="/">Parstudio</router-link>-->
 				</div>
