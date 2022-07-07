927724a345727b536201e64233e7aad04c0d0c55
jh.park <parstudio.kr@gmail.com>
2022-07-07 23:17:04 +0900

    폰트어썸 적용

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,7 +2,11 @@
 	<!-- https://parstudiokr.wixsite.com/parstudio 포트폴리오사이트-->
 	<!-- https://saybyuk.com/  참고사이트-->
 	<!-- http://o-r-k-r.kr/ 참고사이트-->
-	<!-- 220629 김혜련 잠들다. -->
+
+	<font-awesome-icon icon="fa-solid fa-user-secret" />
+	<font-awesome-icon icon="fa-brands fa-twitter" />
+	<font-awesome-icon icon="fa-regular fa-star" />
+
 	<router-view></router-view>
 </template>
 
--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,4 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(fas, fab, far);
+
+/* add font awesome icon component */
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.config.productionTip = false;
+
+app.use(store).use(router).mount("#app");
