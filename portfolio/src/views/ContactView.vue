f4fa19d7a8616e1be85824dc36a1ffafb3852a1b
alieen97 <93559394+alieen97@users.noreply.github.com>
2022-07-16 18:08:23 +0900

    220716 수정

--- portfolio/src/views/ContactView.vue
+++ portfolio/src/views/ContactView.vue
@@ -7,14 +7,12 @@
 				<div class="leftBox">
 					<ul>
 						<li>
-							<font-awesome-icon icon="fa-solid fa-mobile-retro" />
+							<font-awesome-icon icon="fa-solid fa-phone" />
 							<p>+82 10 4560 0488</p>
-							<span class="underLine"></span>
 						</li>
 						<li>
 							<font-awesome-icon icon="fa-solid fa-envelope" />
 							<a href="parstudio.kr@gmail.com">parstudio.kr@gmail.com</a>
-							<span class="underLine"></span>
 						</li>
 						<li>
 							<font-awesome-icon icon="fa-brands fa-instagram-square" />
@@ -22,7 +20,6 @@
 							<a href="https://www.instagram.com/parstudio.kr/">
 								@parstudio.kr
 							</a>
-							<span class="underLine"></span>
 						</li>
 					</ul>
 				</div>
@@ -30,9 +27,13 @@
 			<div class="right">
 				<div class="hoverBack"></div>
 				<div class="inputWrap">
-					<input type="text" placeholder="enter your name..." />
-					<textarea cols="50" rows="13"></textarea>
-					<input type="submit" value="Submit" />
+					<div class="inputTop">
+						<h3>New Question</h3>
+						<button title="submit"><font-awesome-icon icon="fa-solid fa-arrow-up" /></button>
+					</div>
+					<input type="email" placeholder="enter your email..." />
+					<input type="text" placeholder="enter the name..." />
+					<textarea cols="50" rows="13" placeholder="blah blah" style="resize: none;"></textarea>
 					<!--<router-link to="/">Parstudio</router-link>-->
 				</div>
 			</div>
